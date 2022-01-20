be814c8b63ae146e708eabf8c0eb7b71ea3b52a5
David Reyes <davizi96@hotmail.com>
2022-01-20 15:12:53 -0300

    Second practice projec finished

--- 04-second-practice-project/src/components/Cart/Cart.js
+++ 04-second-practice-project/src/components/Cart/Cart.js
@@ -1,11 +1,35 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartAddItemHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartRemoveItemHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={Math.random()}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartRemoveItemHandler.bind(null, item.id)}
+          onAdd={cartAddItemHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -15,13 +39,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- 04-second-practice-project/src/components/UI/Input.js
+++ 04-second-practice-project/src/components/UI/Input.js
@@ -1,12 +1,15 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+// eslint-disable-next-line react/display-name
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
