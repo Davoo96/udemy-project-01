4f71c97d5ca96b694100f635ef69118cc20ce1c8
David Reyes <davizi96@hotmail.com>
2022-03-15 00:01:03 -0300

    Adding Http & Forms

--- 04-second-practice-project/src/store/CartProvider.js
+++ 04-second-practice-project/src/store/CartProvider.js
@@ -55,6 +55,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -72,11 +76,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE_ITEM", id: id });
   };
 
+  const clearCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
--- 04-second-practice-project/src/store/cart-context.js
+++ 04-second-practice-project/src/store/cart-context.js
@@ -3,8 +3,9 @@ import React from "react";
 const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
-  addItem: (item) => {},
-  removeItem: (id) => {},
+  addItem: () => {},
+  removeItem: () => {},
+  clearCart: () => {},
 });
 
 export default CartContext;
